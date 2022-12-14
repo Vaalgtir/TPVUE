f3986f02598b4f83b1e5630a9f116d7832ecc7a1
Vaalgtir <e.eniona2@gmail.com>
2022-12-14 12:36:53 +0100

    init

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -4,15 +4,42 @@ import { data as dataKey } from "./provider/DataProviderKeys";
 
 const props = defineProps(["name"]);
 
-const data = inject(dataKey);
+const dataValue = inject(dataKey);
 
-const value = data[props.name];
+const value = dataValue[props.name];
 
 const handleChange = (event) => {
-  data[props.name] = event.target.value;
+  dataValue[props.name] = event.target.value;
 };
+
+const options = [
+  {
+    name: "zefze",
+    component: {
+      template: "<div>sd;jhgksu</div>",
+    },
+  },
+  {
+    name: "input",
+    component: {
+      template: "<div>hcgsduygc</div>",
+    },
+  },
+];
+
+const currentOption =
+  options.find((opt) => opt.name === props.name) ||
+  options.find((opt) => opt.name === "input");
+
+console.log(currentOption);
 </script>
 
 <template>
-  <input :value="value" @input="handleChange" />
+  <!-- <input :value="value" @input="handleChange" /> -->
+  <!-- {{ currentOption.component }} -->
+  <component
+    v-bind:is="currentOption.component"
+    :value="value"
+    @input="handleChange"
+  ></component>
 </template>
