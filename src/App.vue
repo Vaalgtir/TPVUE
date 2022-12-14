51949c3f2744bf1042e22a1081e42d3c3f41440b
Vaalgtir <e.eniona2@gmail.com>
2022-12-14 11:38:45 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup>
 import Formik from "./components/Formik.vue";
+
+const submit = (data) => {
+  console.log("debug here", data);
+};
 </script>
 
 <template>
   <main>
-    <Formik />
+    <Formik @submitFinal="submit" :validate="() => {}" v-slot="{ handleSubmit }"
+      ><form @submit.prevent="handleSubmit">
+        <button type="submit">sdgdsreg</button>
+      </form>
+    </Formik>
   </main>
 </template>
 
--- src/components/Formik.vue
+++ src/components/Formik.vue
@@ -1,3 +1,17 @@
-<script setup></script>
+<script setup>
+const $emit = defineEmits(["submitFinal"]);
+const props = defineProps({
+  validate: {
+    type: Function,
+    required: true,
+  },
+});
 
-<template><div>jrksdlfh</div></template>
+const handleSubmit = () => {
+  $emit("submitFinal", "wdrjhkfgqze ");
+};
+</script>
+
+<template>
+  <slot :handleSubmit="handleSubmit"></slot>
+</template>
